ce30372b2ff2a3c1844688259c1e6f4a131d2bf9
TomAguero <TomAguero@Gmail.com>
2023-07-23 13:17:27 -0400

    Woohoo, button works! Now to figure out the default text

--- app/src/main/java/com/example/peptalkgenerator/MainActivity.kt
+++ app/src/main/java/com/example/peptalkgenerator/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.example.peptalkgenerator
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.peptalkgenerator.ui.theme.PepTalkGeneratorTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,47 +33,86 @@ override fun onCreate(savedInstanceState: Bundle?) {
     setContent {
         PepTalkGeneratorTheme {
             // A surface container using the 'background' color from the theme
-            Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                GetPrepTalk()                    }
-                val sendIntent: Intent = Intent().apply {
-                    action = Intent.ACTION_SEND
-                    putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
-                    type = "text/plain"
-                }
-
-                val shareIntent = Intent.createChooser(sendIntent, null)
-                startActivity(shareIntent)
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                PepTalkApp()
+            }
             }
         }
     }
 }
 
-
-@Composable
-fun GetPrepTalk() {
-    Column {
-        val greeting: Array<String> = stringArrayResource(R.array.greetings_array)
-        val randomGreeting = greeting.random()
-        Text(randomGreeting)
-
-        val first: Array<String> = stringArrayResource(R.array.first_part_array)
-        val randomFirst = first.random()
-        Text(randomFirst)
-
-        val second: Array<String> = stringArrayResource(R.array.second_part_array)
-        val randomSecond = second.random()
-        Text(randomSecond)
-
-        val salutation: Array<String> = stringArrayResource(R.array.salutations_array)
-        val randomSalutation = salutation.random()
-        Text(randomSalutation)
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
-fun PepTalkPreview() {
-    PepTalkGeneratorTheme {
-        GetPrepTalk()
+fun PepTalkApp() {
+    GetPepTalk(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
+}
+
+
+@Composable
+fun GetPepTalk(modifier: Modifier = Modifier) {
+    var randomGreeting by remember {mutableStateOf("Greeting")}
+    var randomFirst by remember {mutableStateOf("First")}
+    var randomSecond by remember {mutableStateOf("Second")}
+    var randomSalutation by remember {mutableStateOf("Salutation")}
+
+    //region region get random strings
+    //Get random greeting
+    val greeting: Array<String> = stringArrayResource(R.array.greetings_array)
+    //val randomGreeting = greeting.random()
+
+    //Get random first part of pep talk
+    val first: Array<String> = stringArrayResource(R.array.first_part_array)
+    //val randomFirst = first.random()
+
+    //Get random second part
+    val second: Array<String> = stringArrayResource(R.array.second_part_array)
+    //val randomSecond = second.random()
+
+    //Get random salutation
+    val salutation: Array<String> = stringArrayResource(R.array.salutations_array)
+    //val randomSalutation = salutation.random()
+    //endregion
+
+    //put it all together in a column
+    Column(
+        modifier = Modifier
+            .background(color = Color.Black)
+            .padding(4.dp)
+    ) {
+        Text(
+            randomGreeting,
+            color = Color.White
+        )
+        Text(
+            randomFirst,
+            color = Color.White
+        )
+        Text(
+            randomSecond,
+            color = Color.White
+        )
+        Text(
+            randomSalutation,
+            color = Color.White
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            randomGreeting = greeting.random()
+            randomFirst = first.random()
+            randomSecond = second.random()
+            randomSalutation = salutation.random()
+        }) {
+            Text(stringResource(R.string.generate_new))
+        }
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
+
